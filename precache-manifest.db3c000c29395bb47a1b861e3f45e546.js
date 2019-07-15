e6a24ef0f881fd2639b5f226abd7ad566363d430
jemaltahir <jemalmtahir@gmail.com>
2019-07-15 23:24:17 +0300

    Updates

---
+++ precache-manifest.db3c000c29395bb47a1b861e3f45e546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe1e6265bf4d50b69bff1448db9b9bf",
+    "url": "/jemaltahir/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4ac3d517a94385eb712",
+    "url": "/jemaltahir/monsters-rolodex/static/css/main.3f838820.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/jemaltahir/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e4ac3d517a94385eb712",
+    "url": "/jemaltahir/monsters-rolodex/static/js/main.4892ffaa.chunk.js"
+  },
+  {
+    "revision": "82f6b83762077dc9649a",
+    "url": "/jemaltahir/monsters-rolodex/static/js/runtime~main.b33ac17f.js"
+  }
+]);
